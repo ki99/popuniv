b8fcea321a2242e2294ef7be6b97753b2f030764
ki99 <qldntdma87@kaist.ac.kr>
2024-01-23 22:11:01 +0900

    minor fixes

--- popuniv-react/src/pages/DashBoard.js
+++ popuniv-react/src/pages/DashBoard.js
@@ -2,11 +2,13 @@ import React, { useEffect, useState, useCallback } from 'react';
 
 
 export default function DashBoard({dashboardData}) {
+  const sortedData = Object.entries(dashboardData).sort((a, b) => b[1] - a[1]);
+
   return (
     <div>
       <h1>대시보드</h1>
       <ul>
-        {Object.entries(dashboardData).map(([key, value]) => (
+        {sortedData.map(([key, value]) => (
           <li key={key}>
             {key}: {value}
           </li>
