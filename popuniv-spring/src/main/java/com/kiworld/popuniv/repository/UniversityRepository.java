432db8211eea98a420547f8868efc48b6eaab04d
cs20180039 <qldntedma87@kaist.ac.kr>
2024-02-05 01:36:10 +0900

    WIP : controller , Entity 정비

--- popuniv-spring/src/main/java/com/kiworld/popuniv/repository/UniversityRepository.java
+++ popuniv-spring/src/main/java/com/kiworld/popuniv/repository/UniversityRepository.java
@@ -1,9 +1,5 @@
 package com.kiworld.popuniv.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kiworld.popuniv.entity.University;
-
-public interface UniversityRepository extends JpaRepository<University, Integer>{
+public interface UniversityRepository extends SuborganizationRepository {
   
 }
