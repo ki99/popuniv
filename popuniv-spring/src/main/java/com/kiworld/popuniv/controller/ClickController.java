95794af51ec107683db0587d9e71250bed6783de
cs20180039 <qldntedma87@kaist.ac.kr>
2024-02-04 21:47:15 +0900

    configure swagger

--- popuniv-spring/src/main/java/com/kiworld/popuniv/controller/ClickController.java
+++ popuniv-spring/src/main/java/com/kiworld/popuniv/controller/ClickController.java
@@ -1,20 +1,26 @@
 package com.kiworld.popuniv.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.apache.catalina.connector.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import java.util.Map;
 
+@Tag(name = "데모", description = "swagger 데모 api 입니다.")
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/api")
 public class ClickController {
   private static final Logger logger = LoggerFactory.getLogger(ClickController.class);
   private final RedisTemplate<String, Long> redisTemplate;
@@ -23,15 +29,16 @@ public class ClickController {
     this.redisTemplate = redisTemplate;
   }
 
-  @GetMapping("/api/universities/{university_name}/clicks")
+  @GetMapping("/universities/{university_name}/clicks")
+  @Operation(summary = "University의 전체 click 개수의 총합", description = "Get the number of clicks for a university")
   public Long getClicks(@PathVariable("university_name") String universityName) {
     ValueOperations<String, Long> valueOperations = redisTemplate.opsForValue();
     String key = universityName + "_clicks";
     Long clicks = valueOperations.get(key);
     return clicks;
   }
-
-  @PostMapping("/api/universities/{university_name}/clicks")
+  @Operation(summary = "University에 대한 click 개수 반영하기", description = "Post the number of clicks for a university")
+  @PostMapping("/universities/{university_name}/clicks")
   public Long recordClicks(@PathVariable("university_name") String universityName, @RequestBody Map<String, Long> requestBody) {
     long clickCount = requestBody.get("clickCount");
 
--- popuniv-spring/src/main/java/com/kiworld/popuniv/entity/University.java
+++ popuniv-spring/src/main/java/com/kiworld/popuniv/entity/University.java
@@ -1,5 +1,6 @@
 package com.kiworld.popuniv.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +12,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Schema(description = "대학교 정보")
 public class University {
   // make university entity
   @Id //Primary key
