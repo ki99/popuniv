20694bab2eec63ee7205b2e81501928ae2444711
hee-suh <suhee0206@gmail.com>
2024-04-04 15:51:36 +0900

    feat(web): implement nextjs health check

---
+++ popuniv-web/app/api/healthcheck/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+	const healthcheck = {
+		uptime: process.uptime(),
+		message: 'OK',
+		timestamp: Date.now(),
+	};
+	return NextResponse.json(healthcheck);
+}
