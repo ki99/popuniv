effd0a3bff4f27e7ec151ac35a5117e6c323d1e4
ki99 <qldntdma87@kaist.ac.kr>
2024-05-06 20:55:56 +0900

    initialize springboot project with kotlin

---
+++ popuniv-kotlin/src/main/kotlin/org/example/popunivkotlin/PopunivKotlinApplication.kt
@@ -0,0 +1,11 @@
+package org.example.popunivkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PopunivKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<PopunivKotlinApplication>(*args)
+}
---
+++ popuniv-kotlin/src/test/kotlin/org/example/popunivkotlin/PopunivKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.popunivkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PopunivKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
