4a4df32a64309ec6f51b4ed851dfc1ecf5a87754
ki99 <qldntdma87@kaist.ac.kr>
2024-05-15 20:31:59 +0900

    move init.sql to initializer

--- popuniv-kotlin/init.sql
+++
@@ -1,4 +0,0 @@
-create database db;
-use db;
-insert into university (name) values ("POPUNIV");
-// admininitialization 필요
--- popuniv-kotlin/src/main/kotlin/org/example/popunivkotlin/util/Initializer.kt
+++ popuniv-kotlin/src/main/kotlin/org/example/popunivkotlin/util/Initializer.kt
@@ -1,7 +1,6 @@
 package org.example.popunivkotlin.util
 
-import org.example.popunivkotlin.common.Role
-import org.example.popunivkotlin.entity.User
+import org.example.popunivkotlin.repository.UniversityRepository
 import org.example.popunivkotlin.repository.UserRepository
 import org.springframework.boot.ApplicationArguments
 import org.springframework.boot.ApplicationRunner
@@ -9,11 +8,13 @@ import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Component
 
 @Component
-class AdminInitializer(
+class Initializer(
     private val userRepository: UserRepository,
+    private val universityRepository: UniversityRepository,
     private val encoder: PasswordEncoder
 ) : ApplicationRunner {
     override fun run(args: ApplicationArguments?) {
+        // universityRepository.save(University("popuniv"))
         // userRepository.save(User("admin@admin.com", encoder.encode("1admin2"), null, "admin", Role.ADMIN))
     }
 }
