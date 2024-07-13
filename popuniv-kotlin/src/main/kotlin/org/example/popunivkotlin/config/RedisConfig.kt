d4b029c2e3711bb59d3f8ffbb90b4de697419679
ki99 <qldntdma87@kaist.ac.kr>
2024-07-13 22:36:46 +0900

    add passwd to redisconfig

--- popuniv-kotlin/src/main/kotlin/org/example/popunivkotlin/config/RedisConfig.kt
+++ popuniv-kotlin/src/main/kotlin/org/example/popunivkotlin/config/RedisConfig.kt
@@ -3,6 +3,7 @@ package org.example.popunivkotlin.config
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
@@ -12,11 +13,17 @@ import org.springframework.data.redis.serializer.StringRedisSerializer
 @Configuration
 class RedisConfig(
     @Value("\${spring.data.redis.host}") private val host: String,
-    @Value("\${spring.data.redis.port}") private val port: Int
+    @Value("\${spring.data.redis.port}") private val pt: Int,
+    @Value("\${spring.data.redis.password}") private val passwd: String
 ) {
     @Bean
     fun lettuceConnectionFactory(): LettuceConnectionFactory {
-        return LettuceConnectionFactory(RedisStandaloneConfiguration(host, port))
+        val redisStandaloneConfiguration = RedisStandaloneConfiguration().apply {
+            hostName = host
+            port = pt
+            password = RedisPassword.of(passwd)
+        }
+        return LettuceConnectionFactory(redisStandaloneConfiguration)
     }
 
     @Bean
